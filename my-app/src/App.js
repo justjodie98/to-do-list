21930fe8c666d233922c9ea69a7df9c863eb8068
Jodie K <jodieek98@yahoo.co.uk>
2023-09-26 23:11:36 +0100

    Add styling to UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "./Title";
 import { Checkbox } from "./Checkbox";
 import styled from "styled-components";
+import { BsCheckSquareFill } from "react-icons/bs";
 
 const Container = styled.div`
   display: flex;
@@ -9,28 +10,30 @@ const Container = styled.div`
   flex-direction: column;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  color: white;
 `;
 
 const Button = styled.button`
+  color: white;
+  font-size: 1rem;
   display: inline-block;
   flex: 1;
-  border: white;
-  background-color: #219ebc;
-  color: white;
-  height: 70px;
-  width: 100px;
-  border-radius: 2px;
+  border: 2px solid white;
+  border-radius: 25px;
   cursor: pointer;
+  margin-top: 20px;
+  padding: 10px 20px 10px;
 `;
 
 const Text = styled.input`
-  border: 2px solid #000;
+  border: 2px solid white  
+  font-size: 1.25rem;
 `;
 
 const Tasks = styled.div``;
 
 const TaskCount = styled.span`
-  margin: 10px;
+  margin: 25px;
 `;
 
 const LIST = styled.li`
@@ -59,7 +62,7 @@ export default function App() {
   const handleComplete = (id) => {
     let list = todoList.map((task) => {
       let item = {};
-      if (task.id == id) {
+      if (task.id === id) {
         if (!task.complete) {
           //Task is pending, modifying it to complete and increment the count
           setCompletedTaskCount(completedTaskCount + 1);
@@ -74,30 +77,31 @@ export default function App() {
     setTodoList(list);
   };
 
-  const checkbox = (id) => {
-    const checkTask = todoList.filter((task) => {
-      return task.id !== id;
-    });
-    setTodoList(checkTask);
-  };
-
   return (
     <div className="App">
       <Container>
         <Title />
-        <Text value={input} onInput={(e) => setInput(e.target.value)} />
-        <Button disabled={!input} onClick={() => handleClick()}>
-          Add task{" "}
+        <Text
+          style={{ width: "35%", height: "40px" }}
+          value={input}
+          onInput={(e) => setInput(e.target.value)}
+        />
+        <Button
+          className="button"
+          disabled={!input}
+          onClick={() => handleClick()}
+        >
+          Add task
         </Button>
         <Tasks>
           <TaskCount>
             <b>Current Tasks</b> {todoList.length - completedTaskCount}
           </TaskCount>
-          <TaskCount>
-            <b>Completed Tasks</b> {completedTaskCount}
+          <TaskCount style={{ color: "#2dc653" }}> 
+            <b>Completed Tasks</b> {completedTaskCount} <BsCheckSquareFill/>
           </TaskCount>
         </Tasks>
-        <div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <ul>
             {todoList.map((todo) => {
               return (
@@ -105,15 +109,16 @@ export default function App() {
                   complete={todo.complete}
                   id={todo.id}
                   style={{
+                    width: "100%",
                     listStyle: "none",
                     textDecoration: todo.complete && "line-through",
                   }}
                 >
-                  {todo.task}
                   <Checkbox
                     onClick={() => handleComplete(todo.id)}
                     defaultChecked={todo.complete}
                   />
+                  {todo.task}
                 </LIST>
               );
             })}
--- my-app/src/Title.js
+++ my-app/src/Title.js
@@ -1,9 +1,7 @@
-function Title() {
-    return (
-      <div className="Title">
-        <h1>To Do List</h1>
-      </div>
-    );
-  }
-  
-  export default Title;
+export default function Title() {
+  return (
+    <div className="Title">
+      <h1 style={{ color: "white" }}>To Do List</h1>
+    </div>
+  );
+}
