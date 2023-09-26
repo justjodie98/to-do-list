92e03c695a4e44105c895edb3cf3980e99ca09a5
Jodie K <jodieek98@yahoo.co.uk>
2023-09-26 17:55:06 +0100

    Add toDoList functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
+import { useState } from "react";
 import Title from "./Title";
-import Task from "./Task";
 import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 `;
 
 const Button = styled.button`
-font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   display: inline-block;
   flex: 1;
   border: white;
@@ -26,14 +26,86 @@ const Text = styled.input`
   border: 2px solid #000;
 `;
 
+const Tasks = styled.div``;
+
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+
+const LIST = styled.li`
+  liststyle: "none";
+  text-decoration: "line-through";
+`;
+
 export default function App() {
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
+
+  const handleClick = () => {
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+        complete: false,
+      },
+    ]);
+    setInput("");
+  };
+
+  const handleComplete = (id) => {
+    let list = todoList.map((task) => {
+      let item = {};
+      if (task.id == id) {
+        if (!task.complete) {
+          //Task is pending, modifying it to complete and increment the count
+          setCompletedTaskCount(completedTaskCount + 1);
+        } else {
+          //Task is complete, modifying it back to pending, decrement Complete count
+          setCompletedTaskCount(completedTaskCount - 1);
+        }
+        item = { ...task, complete: !task.complete };
+      } else item = { ...task };
+      return item;
+    });
+    setTodoList(list);
+  };
+
   return (
     <div className="App">
       <Container>
         <Title />
-        <Button>Add task</Button>
-        <Text/>
-        <Task />
+        <Button onClick={() => handleClick()}>Add task </Button>
+        <Text value={input} onInput={(e) => setInput(e.target.value)} />
+        <Tasks>
+          <TaskCount>
+            <b>Pending Tasks</b> {todoList.length - completedTaskCount}
+          </TaskCount>
+          <TaskCount>
+            <b>Completed Tasks</b> {completedTaskCount}
+          </TaskCount>
+        </Tasks>
+        <div>
+          <ul>
+            {todoList.map((todo) => {
+              return (
+                <LIST
+                  complete={todo.complete}
+                  id={todo.id}
+                  onClick={() => handleComplete(todo.id)}
+                  style={{
+                    listStyle: "none",
+                    textDecoration: todo.complete && "line-through",
+                  }}
+                >
+                  {todo.task}
+                </LIST>
+              );
+            })}
+          </ul>
+        </div>
         <Button>Clear task</Button>
       </Container>
     </div>
